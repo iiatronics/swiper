2d9aa971146d6303ba0e19fa4e14fe1baf294a75
tallchildvi <andriilevashov@knu.ua>
2026-05-25 16:48:10 +0300

    update prefix in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.feed import router as feed_router
 
-app = FastAPI(title="Music Tinder API")
+app = FastAPI(title="Swiper API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
@@ -13,4 +13,4 @@ app.add_middleware(
 )
 
 # add endpoints
-app.include_router(feed_router, prefix="/api")
+app.include_router(feed_router, prefix="/api/v1")
