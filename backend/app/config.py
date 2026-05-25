44e85a8ec72749a4d0af5fc79a72f5431fc87341
tallchildvi <andriilevashov@knu.ua>
2026-05-25 15:48:40 +0300

    add config and vector_bd

---
+++ backend/app/config.py
@@ -0,0 +1,6 @@
+import os
+
+LATENT_DIM = 128
+ALPHA = 0.8
+INDEX_PATH = os.path.join(os.path.dirname(__file__), "../data/spotify_tracks.index")
+METADATA_PATH = os.path.join(os.path.dirname(__file__), "../data/metadata_mapping.csv")
---
+++ backend/app/services/vector_bd.py
@@ -0,0 +1,22 @@
+import faiss
+import pandas as pd
+from app.config import INDEX_PATH, METADATA_PATH
+
+class VectorDBService:
+    def __init__(self):
+        self.index = faiss.read_index(INDEX_PATH)
+        self.metadata = pd.read_csv(METADATA_PATH)
+
+    def search_neighbors(self, user_vector, k=150):
+        scores, ids = self.index.search(user_vector, k)
+        return ids[0].tolist()
+
+    def get_tracks_metadata(self, track_ids):
+        # Швидкий лукап метаданих треків за списком ID
+        return self.metadata[self.metadata['faiss_id'].isin(track_ids)]
+
+    def reconstruct_vector(self, track_id):
+        return self.index.reconstruct(int(track_id))
+
+# Створюємо синглтон для використання в усьому додатку
+vector_db = VectorDBService()
