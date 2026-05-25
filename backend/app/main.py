42727aa442fb56576b4c82de0bc3a9059b952435
tallchildvi <andriilevashov@knu.ua>
2026-05-25 16:29:25 +0300

    add main script

---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.v1.feed import router as feed_router
+
+app = FastAPI(title="Music Tinder API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# add endpoints
+app.include_router(feed_router, prefix="/api")
